eb7a5860819c7d4937b60ef0d23f948a96995b09
Jimsy2 <dut@vollbio.de>
2022-08-12 23:27:40 +0200

    Create 52 Einen Wert erhöhen.swift

---
+++ Los gehts mit Code/52 Einen Wert erhöhen.swift
@@ -0,0 +1,57 @@
+func turnAround() { 
+    turnLeft(); turnLeft() }
+/*
+func toggleBluePortal() {
+    bluePortal.isActive = !bluePortal.isActive }
+func getGem() {
+    if isOnGem {
+        collectGem()
+        turnAround()
+        toggleBluePortal()
+    } else if isBlocked {
+        turnAround()
+        pinkPortal.isActive = false
+        getGem()
+    } else {
+        moveForward()
+        getGem()
+    }
+}
+for i in 1 ... 4 {
+    getGem()
+}
+
+*/
+func p(_ onoff:Bool) {
+    bluePortal.isActive = onoff
+    pinkPortal.isActive = onoff 
+}
+func mc() {
+    moveForward()
+    cG()
+}
+func cG() {
+    if isOnGem {
+        collectGem()
+    }
+}
+
+p(false)
+mc()
+mc()
+mc()
+turnAround()
+mc()
+p(true)
+mc()
+p(false)
+mc()
+turnAround()
+mc()
+mc()
+turnAround()
+p(true)
+mc()
+turnAround()
+mc()
+mc()
