0cd8c583f2e13cd2f86a30d6f12e8e94721dda8a
Jimsy2 <dut@vollbio.de>
2022-08-12 14:06:30 +0200

    Update 08 Einen Array aus Bildern platzieren.swift

--- Blus Abenteuer/08 Einen Array aus Bildern platzieren.swift
+++ Blus Abenteuer/08 Einen Array aus Bildern platzieren.swift
@@ -6,34 +6,34 @@ scene.place(blu, at:Point(x:-270, y:225))
 // Byte links daneben
 let byte = Graphic(image:#imageLiteral(resourceName: "Byte2@2x.png"))
 byte.scale = 0.35
-scene.place(byte, at:Point(x:-375,y:225))
+scene.place(byte, at:Point(x:-375, y:225))
 // Sonne
 let sun = Graphic(text:"☀️")
  sun.scale = 7
  sun.rotation = -20
- scene.place(sun, at:Point(x:-375,y:-375))
+ scene.place(sun, at:Point(x:-275, y:-375))
 // Planeten
 let emoji = ["🌖","⚪️","🌍","🟠"," 🪩 ","🪐"]
 let dreh = [0  ,  90,   15,  90,    0,  30]
-let size = [0.5,   0.7,  1 ,  0.6,  5,   5]
-let name=[" Me","V","E"," Ma","J","S"]
+let size = [0.5,   0.7,  1.0,  0.6,  5.0, 5.0]
+let name = [" Merkur"," Venus","Erde"," Mars","Jupiter","Saturn"]
 // Position für ersten Planeten recht-oben von der Sonne
-position = Point(x:-290,y:-290)
+position = Point(x:-290, y:-290)
 for i in 1...emoji.count {
-    // Eine Grafik mit Bild erstellen.
-    let g = Graphic(text: emoji[i-1])
+    // Eine Computergrafik aus Text erstellen.
+    let cg = Graphic(text:emoji[i-1])
     // Grafik Skalierung und Drehung festlegen
-    g.scale = size[i-1]
-    g.rotation = dreh[i-1]
+    cg.scale = size[i-1]
+    cg.rotation = dreh[i-1]
     // Die Position für die Grafik festlegen.
-    position.y += 50 + size[i-1]*30
+    position.y += 50 + size[i-1] * 32
     position.x = position.y * 0.7
     // Grafik in der Szene platzieren.
-    scene.place(g, at: position)
+    scene.place(cg, at:position)
     // Beschriftung erstellen und platzieren
     let nameCG = Graphic(text: name[i-1])
     nameCG.scale = 0.67
     var namePos = position
-    namePos.x += size[i-1]*15+25
+    namePos.x += size[i-1]*20+35
     scene.place(nameCG, at:namePos)
 }
